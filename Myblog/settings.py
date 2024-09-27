226132c1e640d10bcafc7ccbbc7ffb65ff92b9fa
Victor Hugo Ferreira <victorhugofbr@gmail.com>
2024-09-27 10:58:36 -0300

    estilos commit

--- Myblog/settings.py
+++ Myblog/settings.py
@@ -127,9 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Diretório global para arquivos estáticos (se houver)
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
